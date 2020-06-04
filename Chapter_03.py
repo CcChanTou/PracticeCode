f7aae7b1788c48de2d1a998d740b00ce064c1cde
CcChanTou <49701520+CcChanTou@users.noreply.github.com>
2020-06-04 15:16:34 +0800

    Add files via upload

---
+++ Chapter_03.py
@@ -0,0 +1,37 @@
+# 01 对数值取整
+# round(value, ndigits)
+# 固定位数输出 format
+
+# 02 执行精确的小数计算
+# decimal
+
+# 对数值做格式化输出
+# format
+
+# 二进制、八进制、十六进制
+# bin() oct() hex()
+
+# 字节串中打包和解包大整数
+# data = b'\x00\x124V\x00x\x90\xab\x00\xcd\xef\x01\x00#\x004'
+# 字节解释为整数 int.from_bytes()
+# 大整数转换为字节串 int.to_bytes()
+
+# 复数计算
+# complex(real, imag)
+
+# 处理无穷大和NaN(not a number)
+# 使用float创建 使用math.isinf() math.isnan()检测
+
+# 分数计算
+# fractions模块
+
+# 处理大型数组的计算
+# NumPy
+
+# 时间换算
+# datetime
+
+
+
+
+
